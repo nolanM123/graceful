0f7e732eb3f15e1d791cfa17e18de34a206e1850
Nolan Michael McAllister <55341005+nolanM123@users.noreply.github.com>
2023-03-24 12:20:42 -0600

    Update graceful.py

--- backend/python/graceful.py
+++ backend/python/graceful.py
@@ -1,11 +1,9 @@
 import json
 import socket
 import asyncio
-import mimetypes
 
 
 class BaseModel:
-
     default: str
 
     def __init__(self, s) -> None:
@@ -18,8 +16,7 @@ class BaseModel:
             self.__setattr__(name, value)
 
 
-class Request:
-
+class HTTPRequest:
     method: str
     url: str
     version: str
@@ -30,13 +27,13 @@ class Request:
     content: bytes
 
     def __init__(self, byte_request: bytes) -> None:
-
         byte_request, self.content = byte_request.rsplit(b"\r\n", 1)
         request_lines = byte_request.decode().split("\r\n")
 
-        self.method, self.url, self.version = request_lines[0].split(" ")
+        self.method, self.url, self.version = request_lines[0].split(" ", 2)
+        self.method = self.method.lower()
 
-        self.queries = {"item": self.content}
+        self.queries = {}
 
         if "?" in self.url:
             self.url, queries = self.url.split("?", 1)
@@ -63,8 +60,7 @@ class Request:
         self.pathvars = {}
 
 
-class Response:
-
+class HTTPResponse:
     closed: bool
     version: str
     status: int
@@ -80,7 +76,6 @@ class Response:
         headers: dict[str, str] = {},
         content: bytes = b"",
     ) -> None:
-
         self.version = "HTTP/1.1"
         self.status = status
         self.reason = reason
@@ -89,7 +84,6 @@ class Response:
         self.content = content
 
     def get_cookie(self, name: str) -> str:
-
         cookie = f"Set-Cookie: {name}={self.cookies[name]['value']}"
 
         if "expires" in self.cookies[name]:
@@ -127,7 +121,6 @@ class Response:
         httponly: bool = False,
         samesite: str = "",
     ) -> None:
-
         self.cookies[name] = {"value": value}
 
         if expires:
@@ -152,11 +145,9 @@ class Response:
             self.cookies[name]["samesite"] = samesite
 
     def del_cookie(self, name: str) -> None:
-
         del self.cookies[name]
 
     def close(self) -> bytes:
-
         status = f"{self.version} {self.status} {self.reason}\r\n"
         headers = "".join(
             f"{name.title()}: {value}\r\n" for name, value in self.headers.items()
@@ -166,22 +157,49 @@ class Response:
         return f"{status}{headers}{cookies}\r\n".encode() + self.content
 
 
-class Graceful:
+class HTTPException(BaseException, HTTPResponse):
+    def __init__(
+        self,
+        status: int = 200,
+        reason: str = "OK",
+        headers: dict[str, str] = {},
+        content: bytes = b"",
+        *args: object,
+    ) -> None:
+        BaseException.__init__(self, *args)
+        HTTPResponse.__init__(self, status, reason, headers, content)
 
+
+class Graceful:
     Timeout: int = 130
     Bufsize: int = 2**10
 
-    def __init__(self, host: str = "localhost", port: int = 8080) -> None:
-
+    def __init__(self, host: str = "localhost", port: int = 8000) -> None:
         self.live: bool = True
         self.host: str = host
         self.port: int = port
-        self.apps: dict[str, object] = {}
+        self.apps: dict[str, object] = {"except": {}}
+
+    async def _closer(self, response: HTTPResponse, result: object) -> bytes:
+        if isinstance(result, HTTPResponse):
+            return result.close()
+
+        if isinstance(result, (tuple, list, dict)):
+            response.headers.setdefault("Content-Type", "application/json")
+            result = json.dumps(result)
+
+        if isinstance(result, str):
+            result = result.encode()
+
+        if isinstance(result, bytes):
+            response.headers.setdefault("Content-Length", len(result))
+            response.content = result
+
+        return response.close()
 
     async def _handler(
-        self, request: Request, response: Response, app: callable
+        self, request: HTTPRequest, response: HTTPResponse, app: callable
     ) -> bytes:
-
         kwargs = {
             "request": request,
             "response": response,
@@ -201,6 +219,7 @@ class Graceful:
                 name.replace("-", "_").lower(): value
                 for name, value in request.pathvars.items()
             },
+            "item": request.content,
         }
 
         for name, value in app.__annotations__.items():
@@ -218,36 +237,12 @@ class Graceful:
             for name, value in kwargs.items()
             if name in app.__code__.co_varnames[: app.__code__.co_argcount]
         }
-        result = (
+
+        return (
             await app(**kwargs) if asyncio.iscoroutinefunction(app) else app(**kwargs)
         )
 
-        if isinstance(result, Response):
-            return result.close()
-
-        if isinstance(result, (tuple, list, dict)):
-            response.headers.setdefault("Content-Type", "application/json")
-            result = json.dumps(result)
-
-        if isinstance(result, str):
-            if response.headers.get("Content-Type") == "text/file":
-                with open(result, "rb") as f:
-                    mimetype, charset = mimetypes.guess_type(result)
-                    response.headers["Content-Type"] = f"{mimetype}; charset={charset}"
-                    result = f.read()
-
-            else:
-                result = result.encode()
-
-        if isinstance(result, bytes):
-            response.headers.setdefault("Content-Type", "application/json")
-            response.headers.setdefault("Content-Length", len(result))
-            response.content = result
-
-        return response.close()
-
-    async def _manager(self, request: Request) -> bytes:
-
+    async def _manager(self, request: HTTPRequest) -> bytes:
         for url, kwargs in self.apps[request.method].items():
             pathvars = {}
             app_url = url.split("/")
@@ -274,20 +269,54 @@ class Graceful:
                 request.pathvars = pathvars
                 app, response, args, kwargs = kwargs.values()
 
-                return await self._handler(request, response(*args, **kwargs), app)
+                return (response(*args, **kwargs), app)
+
+        raise HTTPException(404, "Not Found")
 
     async def _client(self, conn: socket.socket) -> None:
+        with conn:
+            loop = asyncio.get_event_loop()
 
-        loop = asyncio.get_event_loop()
-        data = await asyncio.wait_for(loop.sock_recv(conn, self.Bufsize), self.Timeout)
+            try:
+                request = None
 
-        if data:
-            await loop.sock_sendall(conn, await self._manager(Request(data)))
+                try:
+                    inbytes = await asyncio.wait_for(
+                        loop.sock_recv(conn, self.Bufsize), self.Timeout
+                    )
 
-        conn.close()
+                    if not inbytes:
+                        return
+
+                    request = HTTPRequest(inbytes)
+
+                    while len(request.content) < int(
+                        request.headers.get("Content-Length", 0)
+                    ):
+                        request.content += await asyncio.wait_for(
+                            loop.sock_recv(conn, self.Bufsize), self.Timeout
+                        )
+
+                except TimeoutError:
+                    raise HTTPException(408, "Request Timeout")
+
+                response, app = await self._manager(request)
+                result = await self._handler(request, response, app)
+
+            except BaseException as error:
+                request = request or HTTPRequest(b"   \r\n\r\n")
+                response = (
+                    error
+                    if isinstance(error, HTTPException)
+                    else HTTPException(500, "Internal Server Error")
+                )
+                app = self.apps["except"].get(response.__class__, lambda: None)
+                result = await self._handler(request, response, app)
+
+            outbytes = await self._closer(response, result)
+            await loop.sock_sendall(conn, outbytes)
 
     async def _server(self) -> None:
-
         loop = asyncio.get_event_loop()
 
         while self.live:
@@ -297,7 +326,6 @@ class Graceful:
         self.server.close()
 
     def run(self) -> None:
-
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, self.port))
         self.server.setblocking(False)
@@ -309,10 +337,11 @@ class Graceful:
         self,
         method: str,
         url: str,
-        response: Response = Response,
+        response: HTTPResponse = HTTPResponse,
         *args: tuple[object, ...],
         **kwargs: dict[str, object],
     ) -> callable:
+        method = method.lower()
 
         if method not in self.apps:
             self.apps[method] = dict()
@@ -324,5 +353,14 @@ class Graceful:
                 "args": args,
                 "kwargs": kwargs,
             }
+
+        return routing
+
+    def exception(
+        self,
+        exception: HTTPException,
+    ) -> callable:
+        def routing(app: callable) -> None:
+            self.apps["except"][exception] = app
 
         return routing
